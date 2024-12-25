1317695148ffe4a51331302ef994654f64e172f3
Thilo Schwarz <osp@thischwa.codes>
2024-12-25 17:10:32 +0100

    Remove unused `davPath` property from DAV configuration.

--- src/main/java/codes/thischwa/bcg/conf/DavConf.java
+++ src/main/java/codes/thischwa/bcg/conf/DavConf.java
@@ -1,22 +1,20 @@
 package codes.thischwa.bcg.conf;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.lang.Nullable;
 import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * Configuration properties for DAV integration. These properties are mapped from configuration
  * sources with the prefix `dav`.
  *
- * @param user The username for authentication.
+ * @param user     The username for authentication.
  * @param password The password for authentication.
- * @param davPath Optional path for DAV-specific configurations.
- * @param calUrl The URL for accessing calendar services.
- * @param cardUrl The URL for accessing address book services.
+ * @param calUrl   The URL for accessing calendar services.
+ * @param cardUrl  The URL for accessing address book services.
  */
 @ConfigurationProperties(prefix = "dav")
 public record DavConf(
-    String user, String password, @Nullable String davPath, String calUrl, String cardUrl) {
+        String user, String password, String calUrl, String cardUrl) {
 
   public String getBaseUrl() {
     return UriComponentsBuilder.fromUriString(cardUrl)
